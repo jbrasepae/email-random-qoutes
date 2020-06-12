aa8cf3b3945d4ec1e8fe2b73c31b05672245cae3
Jeanette <jbrasepae@gmail.com>
2020-06-12 14:51:27 +0200

    added code but project still not complete

--- src/send_inspiration.js
+++ src/send_inspiration.js
@@ -5,3 +5,41 @@ function inspirationQuotes(){
     return randomQuote;
 }
 console.log(inspirationQuotes());
+
+// const nodeEmail = require("nodeEmail")
+// const fs = require("fs")
+// const qoutes = fs.readFileSync("./qoutes.txt", "utf-8")
+
+// const {
+//     SMTP_SERVER, SMTP_PORT, SMTP_LOGIN,SMTP_PASSWORD 
+// } = process.env
+
+// function randomInspiritaion(){
+//     var randomQuote = Math.floor(Math.random()*(quotes.length))
+//     return quotes[randomQuote]
+// }
+
+// var transporter = nodeEmail.createTransporter({
+//     host: SMTP_SERVER,
+//     port: SMTP_PORT,
+//     auth:{
+//         service: "gmail",
+//         user: SMTP_LOGIN,
+//         pass:SMTP_PASSWORD
+//     }
+// })
+
+// let emailDetails = {
+//     from: SMTP_LOGIN,
+//     to: "boitumelo.rasepae@umuzi.org",
+//     subject: "Inspirational Quotes",
+//     text: randomInspiritaion()
+// }
+
+// transporter.sendMail(email, function(error,info){
+//     if(error){
+//         console.log(error)
+//     }else{
+//         console.log("email sent: " + info.response)
+//     }
+// })
