2fb8e5b03f1de9be45fc72f60948afcabb836717
Jeanette <jbrasepae@gmail.com>
2020-10-27 16:21:07 +0200

    project not yet complete

--- src/send_inspiration.js
+++ src/send_inspiration.js
@@ -1,45 +1,45 @@
-let quotes = ["The only true measure of success if the number of people you have helped" 
-]
-function inspirationQuotes(){
-    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    return randomQuote;
+// let quotes = ["The only true measure of success if the number of people you have helped" 
+// ]
+// function inspirationQuotes(){
+//     var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+//     return randomQuote;
+// }
+// console.log(inspirationQuotes());
+
+const nodeEmail = require("nodeEmail")
+const fs = require("fs")
+const qoutes = fs.readFileSync("./qoutes.txt", "utf-8")
+
+const {
+    SMTP_SERVER, SMTP_PORT, SMTP_LOGIN,SMTP_PASSWORD 
+} = process.env
+
+function randomInspiritaion(){
+    var randomQuote = Math.floor(Math.random()*(quotes.length))
+    return quotes[randomQuote]
 }
-console.log(inspirationQuotes());
 
-// const nodeEmail = require("nodeEmail")
-// const fs = require("fs")
-// const qoutes = fs.readFileSync("./qoutes.txt", "utf-8")
+var transporter = nodeEmail.createTransporter({
+    host: SMTP_SERVER,
+    port: SMTP_PORT,
+    auth:{
+        service: "gmail",
+        user: SMTP_LOGIN,
+        pass:SMTP_PASSWORD
+    }
+})
 
-// const {
-//     SMTP_SERVER, SMTP_PORT, SMTP_LOGIN,SMTP_PASSWORD 
-// } = process.env
+let emailDetails = {
+    from: SMTP_LOGIN,
+    to: "boitumelo.rasepae@umuzi.org",
+    subject: "Inspirational Quotes",
+    text: randomInspiritaion()
+}
 
-// function randomInspiritaion(){
-//     var randomQuote = Math.floor(Math.random()*(quotes.length))
-//     return quotes[randomQuote]
-// }
-
-// var transporter = nodeEmail.createTransporter({
-//     host: SMTP_SERVER,
-//     port: SMTP_PORT,
-//     auth:{
-//         service: "gmail",
-//         user: SMTP_LOGIN,
-//         pass:SMTP_PASSWORD
-//     }
-// })
-
-// let emailDetails = {
-//     from: SMTP_LOGIN,
-//     to: "boitumelo.rasepae@umuzi.org",
-//     subject: "Inspirational Quotes",
-//     text: randomInspiritaion()
-// }
-
-// transporter.sendMail(email, function(error,info){
-//     if(error){
-//         console.log(error)
-//     }else{
-//         console.log("email sent: " + info.response)
-//     }
-// })
+transporter.sendMail(email, function(error,info){
+    if(error){
+        console.log(error)
+    }else{
+        console.log("email sent: " + info.response)
+    }
+})
